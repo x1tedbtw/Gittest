a02684f8061a87fa6d3d743ebf2df7d8f0f70b7f
x1tedbtw <nefedov.vitali@gmail.com>
2024-12-11 14:48:43 +0100

    inritial commit

---
+++ src/model/Main.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+        System.out.println(a+b);
+        System.out.println("Hello, World!");
+    }
+}
+
+
